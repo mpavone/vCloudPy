a8531c3860045e62dd8bc87144a4152a1129fd21
Martino Pavone <martino.pavone@gmail.com>
2015-01-07 21:41:11 +0100

    Update SampleCopyVApp.py

--- src/SampleCopyVApp.py
+++ src/SampleCopyVApp.py
@@ -1,7 +1,24 @@
+# vCloudPy: VMWare vCloud Automation for Python Devops
+# Copyright (c) 2014 Martino Pavone. All Rights Reserved.
+#
+# Licensed under the MIT License , (the "License"); 
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from vCloud import vCloud
 
 print ("\n--------------------------------------------------------------------")
 print ("vCloudPy: VMWare vCloud Automation for Python Devops")
+print ("--------------------------------------------------------------------")
+print ("This sample shows how to copy an entire vApp")
 print ("--------------------------------------------------------------------")
 
 vCloudClient = vCloud()
@@ -18,7 +35,7 @@ vCloudClient.connect(org, username, password, endpoint)
 print ('\nSelected vApp Destination')
 print ('----------------------------------')
 select = ['name', 'href']
-filter = {'name':'newVA2'}
+filter = {'name':'DESTINATION VAPP'}
 vapps= vCloudClient.get_vapp(select=select,  filter=filter)
 vCloudClient.print_item(vapps,  select=select)
 
@@ -27,7 +44,7 @@ vapp_href_destination = vapps['href']
 print ('\nSelected vApp Source')
 print ('----------------------------------')
 select = ['name', 'href']
-filter = {'name':'newVA7'}
+filter = {'name':'SOURCE VAPP'}
 vapps= vCloudClient.get_vapp(select=select,  filter=filter)
 vCloudClient.print_item(vapps,  select=select)
 
@@ -35,7 +52,7 @@ vapp_href_source= vapps['href']
 
 print ('\nCopy the vapp into the vApp ')
 print ('----------------------------------')
-task = vCloudClient.copy_vapp(vapp_href_destination,  vapp_href_source, 'this is a copied VM2')
+task = vCloudClient.copy_vapp(vapp_href_destination,  vapp_href_source, 'this is copied in the destination vApp')
 vCloudClient.print_item(task)
 vCloudClient.wait_task(task['href'], print_progress=True)
 print ('\nCompleted ')
